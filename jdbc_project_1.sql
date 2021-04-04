da59350273104bdb7cddb874493c1addbb5951ae
김태형 <rlaxogud94@naver.com>
2021-04-05 05:55:52 +0900

    Add files via upload

---
+++ jdbc_project_1.sql
@@ -0,0 +1,149 @@
+-- 멤버 테이블 생성
+
+drop sequence member_sequence;
+drop sequence groups_sequence;
+drop sequence board_sequence;
+drop sequence board_category_sequence;
+drop table member cascade constraints;
+drop table groups cascade constraints;
+drop table group_enroll cascade constraints;
+drop table board_category cascade constraints;
+drop table board cascade constraints;
+drop cluster cls_member_id;
+
+create cluster cls_member_id(member_id number);
+create index cls_member_id_idx on cluster cls_member_id;
+
+
+
+create table member(
+id number,
+email_id varchar2(40),
+password varchar2(20),
+name varchar2(20),
+group_cnt number,
+constraint member_email_nn check(email_id is not null),
+constraint member_password_nn check(password is not null),
+constraint member_name_nn check(name is not null),
+constraint member_group_cnt_nn check(group_cnt is not null)
+) cluster cls_member_id(id);
+
+create index member_id_pk_idx on member(id);
+create index member_email_id_unique_idx on member(email_id);
+alter table member add constraint member_id_pk primary key(id);
+alter table member add constraint member_email_id_unique unique(email_id);
+
+-- 그룹 테이블
+
+create table groups(
+    id number,
+    host_member_id number,
+    name varchar2(20),
+    participate_id varchar(4),
+    constraints groups_name_nn check(name is not null),
+    constraints groups_host_member_id check(host_member_id is not null),
+    constraints groups_participate_id_nn check(participate_id is not null)
+) cluster cls_member_id(host_member_id);
+
+create index groups_id_pk_idx on groups(id);
+create index groups_host_member_id_fk_idx on groups(host_member_id);
+create index groups_participate_id_idx on groups(participate_id);
+
+alter table groups add constraint groups_id_pk primary key(id);
+alter table groups add constraint groups_host_member_id_fk foreign key (host_member_id) references member(id);
+alter table groups add constraint groups_participate_id_unique unique(participate_id);
+
+alter table groups add member_cnt number;
+
+
+-- 그룹 인롤 테이블
+create table group_enroll(
+    member_id number,
+    groups_id number,
+    enroll_date date,
+    constraints member_id_nn check (member_id is not null),
+    constraints groups_id_nn check (groups_id is not null),
+    constraints enroll_date_nn check (enroll_date is not null),
+    constraint group_enroll_member_id_groups_id_pk primary key(member_id, groups_id)
+) organization index;
+
+-- create index group_enroll_member_id_groups_id_pk_idx on group_enroll(member_id, groups_id);
+create index group_enroll_member_id_fk_idx on group_enroll(member_id);
+create index group_enroll_groups_id_fk_idx on group_enroll(groups_id);
+create index group_enroll_date_idx on group_enroll(enroll_date);
+alter table group_enroll add constraint group_enroll_member_id_fk foreign key(member_id) references member(id);
+alter table group_enroll add constraint group_enroll_groups_id_fk foreign key(groups_id) references groups(id);
+
+
+create table board_category(
+    id number,
+    groups_id number,
+    name varchar2(20),
+    board_count number,
+    constraints board_category_groups_id check(groups_id is not null),
+    constraints board_category_name_nn check(name is not null)
+);
+
+create index board_category_id_pk_idx on board_category(id);
+create index board_category_groups_id_fk_idx on board_category(groups_id);
+create index board_category_groups_id_name_uniuqe_idx on board_category(groups_id,name);
+alter table board_category add constraint board_category_id_pk primary key(id);
+alter table board_category add constraint board_category_groups_id_fk foreign key(groups_id) references groups(id);
+alter table board_category add constraint board_category_groups_id_name_unique unique(groups_id,name);
+
+-- board 테이블 생성
+
+create table board(
+    id number,
+    writer_id number,
+    writer_name varchar2(20),
+    groups_id number,
+    board_category_id number,
+    title varchar2(30),
+    content varchar2(2000),
+    write_date date,
+    constraints board_groups_id_nn check(groups_id is not null),
+    constraints board_title_nn check(title is not null),
+    constraints board_content_id_nn check(content is not null)
+)
+partition by range(groups_id)
+(
+    partition pr_board_5 values less than(5),
+    partition pr_board_10 values less than(10)
+);
+
+create index board_id_pk_idx on board(id);
+create index board_writer_id_fk_idx on board(writer_id);
+create index board_groups_id_fk_idx on board(groups_id);
+create index board_board_category_id_fk_idx on board(board_category_id);
+create index board_title_idx on board(title) indextype is ctxsys.context;
+create index board_content_idx on board(content) indextype is ctxsys.context;
+
+alter table board add constraint board_id_pk primary key(id);
+alter table board add constraint board_writer_id_fk foreign key(writer_id) references member(id) on delete set null;
+alter table board add constraint board_groups_id_fk foreign key(groups_id) references groups(id);
+alter table board add constraint board_board_category_id_fk foreign key(board_category_id) references board_category(id);
+alter table board add view_cnt number;
+
+create sequence member_sequence;
+create sequence groups_sequence;
+create sequence board_category_sequence;
+create sequence board_sequence;
+
+create or replace view groups_manage_view 
+as 
+select 
+    g.id group_id, 
+    g.name group_name,
+    g.member_cnt group_member_cnt,
+    m.id group_host_id, 
+    m.name group_host_name, 
+    bc.board_count groups_total_board_cnt  
+from groups g 
+join 
+member m 
+on g.host_member_id = m.id 
+join board_category bc 
+on g.id = bc.groups_id;
+
+create or replace synonym group_syn for groups;
---
+++ jdbc_project_3.sql
@@ -0,0 +1,284 @@
+create or replace package groups_enroll_package -- 그룹 가입 관련 패키지
+is
+    procedure signup_group(p_member_id member.id%type, p_groups_id groups.id%type);
+    procedure signout_group(p_member_id member.id%type, p_groups_id groups.id%type);
+    function get_group_enroll_list_within_week(p_groups_id group_enroll.groups_id%type) return sys_refcursor;
+end;
+/
+
+
+create or replace package body groups_enroll_package -- 그룹 가입 관련 패키지 바디
+is
+    procedure signup_group(p_member_id member.id%type, p_groups_id groups.id%type)
+    is
+    begin
+        insert into group_enroll values(p_member_id, p_groups_id, sysdate);
+        update groups set member_cnt = member_cnt + 1 where id = p_groups_id;
+        commit;
+        exception when others then
+            rollback;
+    end;
+    
+    procedure signout_group(p_member_id member.id%type, p_groups_id groups.id%type)
+    is
+        host_id groups.host_member_id%type;
+    begin
+        select host_member_id into host_id from groups where id = p_groups_id;
+        if host_id = p_member_id then
+            raise_application_error(-20000, 'owner can''t signout from group');
+        end if;
+        update board set writer_id = null, writer_name = '탈퇴회원' where writer_id = p_member_id;
+        delete from group_enroll where member_id = p_member_id and groups_id = p_groups_id;
+        update groups set member_cnt = member_cnt -1 where id = p_groups_id;
+        commit;
+    end;
+    function get_group_enroll_list_within_week(p_groups_id group_enroll.groups_id%type) return sys_refcursor
+    is
+    group_enroll_list sys_refcursor;
+    begin
+        open group_enroll_list for
+            with week_date(day) as (
+                select sysdate - 6 as day
+                from dual
+                union all
+                select day + 1 from week_date where day < sysdate
+            ) select 
+                distinct day, 
+                        decode(enroll_date, null, 0, count(*) over (partition by day)) enroll_member_cnt
+                        from week_date 
+                        left join group_enroll 
+                        on to_date(day,'YY/MM/DD') = to_date(enroll_date, 'YY/MM/DD') and group_enroll.groups_id= p_groups_id
+                        order by day desc;
+            
+        return group_enroll_list;
+    end;
+    
+end;
+/
+
+create or replace package board_category_package
+is
+    procedure make_board_category(p_groups_id in board_category.groups_id%type, p_name in board_category.name%type);
+    procedure get_board_category_list(p_groups_id board_category.groups_id%type, v_board_category_list out sys_refcursor);
+    function get_board_category_count(p_groups_id board_category.groups_id%type, p_board_category_id board_category.id%type) return number;
+end;
+/
+
+create or replace package body board_category_package
+is
+    procedure make_board_category(p_groups_id in board_category.groups_id%type, p_name in board_category.name%type)
+    is
+    begin
+        insert into board_category values(board_category_sequence.nextval, p_groups_id, p_name, 0);
+        commit;
+    end make_board_category;
+    
+    
+    
+    procedure get_board_category_list(p_groups_id board_category.groups_id%type, v_board_category_list out sys_refcursor)
+    is
+    begin
+        open v_board_category_list 
+        for
+        select * from board_category where groups_id = p_groups_id;
+    end get_board_category_list;
+    
+    function get_board_category_count(p_groups_id board_category.groups_id%type, p_board_category_id board_category.id%type) return number
+    is
+        category_count number;
+    begin
+        select count(*) into category_count
+        from
+        board_category
+        where id = p_board_category_id and groups_id = p_groups_id;
+        return category_count;
+    end;
+end;
+/
+
+create or replace package board_package
+is
+    function board_owner_check(p_board_id board.id%type, p_member_id board.writer_id%type, p_groups_id board.groups_id%type) return number;
+    function get_board_cnt_with_category(p_groups_id board.groups_id%type, p_board_category_id board.board_category_id%type) return number;
+    procedure write_board(p_member_id board.writer_id%type, 
+                          p_groups_id board.groups_id%type, 
+                          p_board_category_id board.board_category_id%type,
+                          p_board_title board.title%type,
+                          p_board_content board.content%type);
+    procedure edit_board(p_board_id board.id%type,
+                          p_member_id board.writer_id%type, 
+                          p_groups_id board.groups_id%type, 
+                          p_board_title board.title%type,
+                          p_board_content board.content%type);
+    procedure delete_board(p_member_id board.writer_id%type, p_board_id board.id%type, p_groups_id board.groups_id%type);
+procedure read_board_detail(p_board_id board.id%type, 
+                                v_board_id out board.id%type,
+                                v_writer_id out board.writer_id%type,
+                                v_writer_name out board.writer_name%type,
+                                v_groups_id out board.groups_id%type,
+                                v_board_category_id out board.board_category_id%type,
+                                v_title out board.title%type,
+                                v_content out board.content%type,
+                                v_write_date out board.write_date%type,
+                                v_view_cnt out board.view_cnt%type
+                                );
+    procedure read_board_list_with_paging(p_groups_id board.groups_id%type, p_board_category_id board.board_category_id%type, p_page_number number, board_list out sys_refcursor);
+    
+    function read_board_list_with_searching_and_paging(p_groups_id board.groups_id%type, 
+                                                       p_board_category_id board.board_category_id%type, 
+                                                       p_page_number number, 
+                                                       p_kind varchar2, 
+                                                       p_keyword varchar2) return sys_refcursor;
+end;
+/
+
+create or replace package body board_package
+is
+    function board_owner_check(p_board_id board.id%type, p_member_id board.writer_id%type, p_groups_id board.groups_id%type) return number
+    is
+        res number;
+        v_writer_id board.writer_id%type;
+        v_host_id board.writer_id%type;
+    begin
+        select writer_id into v_writer_id from board where id = p_board_id;
+        select host_member_id into v_host_id from groups where id = p_groups_id;
+        if p_member_id in (v_writer_id, v_host_id) then
+            res := 1;
+        else
+            res := 0;
+        end if;
+        return res;
+        exception when others then
+            return 0;
+    end;
+
+    function get_board_cnt_with_category(p_groups_id board.groups_id%type, p_board_category_id board.board_category_id%type) return number
+    is
+        cnt number := 0;
+    begin
+        select board_count into cnt from board_category where id = p_board_category_id and groups_id = p_groups_id;
+        return cnt;
+    end;
+    
+    function get_groups_host(p_groups_id groups.host_member_id%type) return number
+    is
+        host_id number := 0;
+    begin
+        select host_member_id into host_id from groups where id = p_groups_id;
+        return host_id;
+    end;
+       
+    procedure write_board(p_member_id board.writer_id%type, 
+                          p_groups_id board.groups_id%type, 
+                          p_board_category_id board.board_category_id%type,
+                          p_board_title board.title%type,
+                          p_board_content board.content%type)
+    is
+        member_name member.name%type;
+    begin
+        select name into member_name from member where id = p_member_id;
+        insert into board values(board_sequence.nextval, p_member_id, member_name, p_groups_id, p_board_category_id, p_board_title, p_board_content, sysdate, 0);
+        commit;
+    end write_board;
+    
+    procedure edit_board(p_board_id board.id%type,
+                          p_member_id board.writer_id%type, 
+                          p_groups_id board.groups_id%type, 
+                          p_board_title board.title%type,
+                          p_board_content board.content%type)
+    is
+         flag_number number;
+    begin
+        flag_number := board_owner_check(p_board_id, p_member_id, p_groups_id);
+        if flag_number = 0 then
+            raise_application_error(-20000, 'only owner can control');
+        end if;
+        update 
+        board 
+        set title = p_board_title, content = p_board_content
+        where id = p_board_id;
+        commit;
+    end edit_board;
+    
+    procedure delete_board(p_member_id board.writer_id%type, p_board_id board.id%type, p_groups_id board.groups_id%type)
+    is
+        flag_number number;
+    begin
+        flag_number := board_owner_check(p_board_id, p_member_id, p_groups_id);
+        if flag_number = 0 then
+            raise_application_error(-20000, 'only owner can control');
+        end if;
+        delete 
+        from 
+        board 
+        where 
+        id = p_board_id;
+        commit;
+    end delete_board;
+    
+    procedure read_board_detail(p_board_id board.id%type, 
+                                v_board_id out board.id%type,
+                                v_writer_id out board.writer_id%type,
+                                v_writer_name out board.writer_name%type,
+                                v_groups_id out board.groups_id%type,
+                                v_board_category_id out board.board_category_id%type,
+                                v_title out board.title%type,
+                                v_content out board.content%type,
+                                v_write_date out board.write_date%type,
+                                v_view_cnt out board.view_cnt%type
+                                )
+    is
+    begin
+        update board set view_cnt = view_cnt +1 where id = p_board_id;
+        select
+        id, writer_id, writer_name, groups_id, board_category_id, title, content, write_date, view_cnt
+        into v_board_id, v_writer_id, v_writer_name, v_groups_id, v_board_category_id, v_title, v_content, v_write_date, v_view_cnt
+        from board 
+        where id = p_board_id;
+        commit;
+        exception 
+            when others then
+            rollback;
+    end read_board_detail;
+    
+    procedure read_board_list_with_paging(p_groups_id board.groups_id%type, p_board_category_id board.board_category_id%type, p_page_number number, board_list out sys_refcursor)
+    is
+    begin
+        open board_list for
+            select bnum, id, title, writer_name, write_date, view_cnt from (
+                select * from (
+                    select 
+                        rownum bnum, board.*
+                    from board 
+                    where 
+                    groups_id = p_groups_id and board_category_id = p_board_category_id
+                    order by id
+                    )
+                where bnum > (p_page_number-1)*5
+            ) where rownum <= 5 order by bnum;
+    end read_board_list_with_paging;
+    
+    function read_board_list_with_searching_and_paging(p_groups_id board.groups_id%type, 
+                                                       p_board_category_id board.board_category_id%type, 
+                                                       p_page_number number, 
+                                                       p_kind varchar2, 
+                                                       p_keyword varchar2) return sys_refcursor
+    is
+    board_list_cursor sys_refcursor;
+    q1 varchar2(1000) := 'select bnum, id, title, writer_name, write_date, view_cnt from (
+                select * from (
+                    select 
+                        rownum bnum, board.*
+                    from board 
+                    where 
+                    groups_id = :p_groups_id and board_category_id = :p_board_category_id';
+    q2 varchar2(100) := ' order by id)
+	where bnum > (:p_page_number-1)*5) 
+	where rownum <= 5 order by bnum';
+    begin
+        q1 := q1 || ' and ' || p_kind || ' like ' || '''%' || p_keyword || '%''' || q2;
+        open board_list_cursor for q1 using p_groups_id, p_board_category_id, p_page_number;
+        return board_list_cursor;
+    end read_board_list_with_searching_and_paging;
+end;
+/
